c95061c8ac73653466ebd19e8d6cb6a0b7c2f919
Akriti Singh <akritisingh1811@gmail.com>
2021-05-07 00:51:46 +0530

    added cardview, post, login and sign up pages

--- main.py
+++ main.py
@@ -9,5 +9,21 @@ def buy():
 def rent():
     return render_template('rent.html')
 
+@app.route("/post")
+def post():
+    return render_template('post.html')
+
+@app.route("/cardview")
+def cardview():
+    return render_template('cardview.html')
+   
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+@app.route("/signup")
+def signup():
+    return render_template('signup.html')
+
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
